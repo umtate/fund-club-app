0dd463fad3453928e75a80effc782cd259b969bf
Tatenda Nyamugama <tatenda@astria.co.za>
2024-10-17 13:04:33 +0200

    added yup and formik

--- src/app/components/admin/AddPrdoucts.tsx
+++ src/app/components/admin/AddPrdoucts.tsx
@@ -10,89 +10,196 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object().shape({
+  title: Yup.string().required(),
+  author: Yup.string().required(),
+  isbn: Yup.string().required(),
+  publisher: Yup.string().required(),
+  genre: Yup.string().required(),
+  edition: Yup.string().required(),
+  numberOfPages: Yup.number().required(),
+  quantity: Yup.number().required(),
+  price: Yup.string().required(),
+  picture: Yup.string().required(),
+});
 
 export function AppProductsComponent() {
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      author: "",
+      isbn: "",
+      publisher: "",
+      genre: "",
+      edition: "",
+      numberOfPages: "",
+      quantity: "",
+      price: "",
+      picture: "",
+    },
+    validationSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
     <Card>
       <CardHeader className="space-y-1">
         <CardTitle className="text-2xl">Add book to catalogue</CardTitle>
       </CardHeader>
 
-      <CardContent className="grid gap-6">
-        <div className="flex">
-          <div className="py-1.25 mx-3 px-0.75 items-center  w-1/2 md:w-full">
-            <Label htmlFor="title">Tilte</Label>
-            <Input id="title" type="text" />
+      <form onSubmit={formik.handleSubmit}>
+        <CardContent className="grid gap-6">
+          <div className="flex">
+            <div className="py-1.25 mx-3 px-0.75 items-center  w-1/2 md:w-full">
+              <Label htmlFor="title">Tilte</Label>
+              <Input
+                id="title"
+                type="text"
+                name="title"
+                onChange={formik.handleChange}
+                value={formik.values.title}
+              />
+              {formik.errors.title && formik.touched.title ? (
+                <Label className="text-red-600">{formik.errors.title}</Label>
+              ) : null}{" "}
+            </div>
+            <div className="py-1.25 mx-3 px-0.75 items-center  w-1/2 md:w-full">
+              <Label htmlFor="author">Author</Label>
+              <Input
+                id="author"
+                type="text"
+                name="author"
+                onChange={formik.handleChange}
+                value={formik.values.author}
+              />
+              {formik.errors.author && formik.touched.author ? (
+                <Label className="text-red-600">{formik.errors.author}</Label>
+              ) : null}{" "}
+            </div>
           </div>
-          <div className="py-1.25 mx-3 px-0.75 items-center  w-1/2 md:w-full">
-            <Label htmlFor="author">Author</Label>
-            <Input id="author" type="text" />
-          </div>
-        </div>
 
-        <div className="flex">
-          <div className="py-1.25 mx-3 px-0.75 items-center  w-1/2 md:w-full">
-            <Label htmlFor="isbn">ISBN</Label>
-            <Input id="isbn" type="text" />
+          <div className="flex">
+            <div className="py-1.25 mx-3 px-0.75 items-center  w-1/2 md:w-full">
+              <Label htmlFor="isbn">ISBN</Label>
+              <Input
+                id="isbn"
+                type="text"
+                name="isbn"
+                onChange={formik.handleChange}
+                value={formik.values.isbn}
+              />
+              {formik.errors.isbn && formik.touched.isbn ? (
+                <Label className="text-red-600">{formik.errors.isbn}</Label>
+              ) : null}{" "}
+            </div>
+            <div className="py-1.25 mx-3 px-0.75 items-center  w-1/2 md:w-full">
+              <Label htmlFor="publisher">Publisher</Label>
+              <Input
+                id="publisher"
+                type="text"
+                name="publisher"
+                onChange={formik.handleChange}
+                value={formik.values.publisher}
+              />
+              {formik.errors.publisher && formik.touched.publisher ? (
+                <Label className="text-red-600">
+                  {formik.errors.publisher}
+                </Label>
+              ) : null}{" "}
+            </div>
           </div>
-          <div className="py-1.25 mx-3 px-0.75 items-center  w-1/2 md:w-full">
-            <Label htmlFor="publisher">Publisher</Label>
-            <Input id="publishe" type="text" />
-          </div>
-        </div>
 
-        <div className="flex">
-          <div className="py-1.25 mx-3 px-0.75 items-center  w-1/2 md:w-full">
-            <Label htmlFor="publicationDate">Publication Date</Label>
-            <Input id="publicationDate" type="text" />
-          </div>
-          <div className="py-1.25 mx-3 px-0.75 items-center  w-1/2 md:w-full">
-            <Label htmlFor="genre">Genre/Category</Label>
-            <Input id="genre" type="text" />
-          </div>
-        </div>
+          <div className="flex">
+            <div className="py-1.25 mx-3 px-0.75 items-center  w-1/2 md:w-full">
+              <Label htmlFor="genre">Genre/Category</Label>
+              <Input
+                id="genre"
+                type="text"
+                name="genre"
+                onChange={formik.handleChange}
+                value={formik.values.genre}
+              />
+              {formik.errors.genre && formik.touched.genre ? (
+                <Label className="text-red-600">{formik.errors.genre}</Label>
+              ) : null}{" "}
+            </div>
 
-        <div className="flex">
-          <div className="py-1.25 mx-3 px-0.75 items-center  w-1/2 md:w-full">
-            <Label htmlFor="language">Language</Label>
-            <Input id="language" type="text" />
-          </div>
-          <div className="py-1.25 mx-3 px-0.75 items-center  w-1/2 md:w-full">
-            <Label htmlFor="edition">Edition</Label>
-            <Input id="edition" type="text" />
-          </div>
-        </div>
+            <div className="py-1.25 mx-3 px-0.75 items-center  w-1/2 md:w-full">
+              <Label htmlFor="edition">Edition</Label>
+              <Input
+                id="edition"
+                type="text"
+                name="edition"
+                onChange={formik.handleChange}
+                value={formik.values.edition}
+              />
+              {formik.errors.edition && formik.touched.edition ? (
+                <Label className="text-red-600">{formik.errors.edition}</Label>
+              ) : null}{" "}
+            </div>
 
-        <div className="flex">
-          <div className="py-1.25 mx-3 px-0.75 items-center  w-1/2 md:w-full">
-            <Label htmlFor="numberOfPages">Number of Pages</Label>
-            <Input id="numberOfPages" type="text" />
+            <div className="py-1.25 mx-3 px-0.75 items-center  w-1/2 md:w-full">
+              <Label htmlFor="numberOfPages">Number of Pages</Label>
+              <Input
+                id="numberOfPages"
+                type="number"
+                name="numberOfPages"
+                onChange={formik.handleChange}
+                value={formik.values.numberOfPages}
+              />
+              {formik.errors.numberOfPages && formik.touched.numberOfPages ? (
+                <Label className="text-red-600">
+                  {formik.errors.numberOfPages}
+                </Label>
+              ) : null}{" "}
+            </div>
           </div>
-          <div className="py-1.25 mx-3 px-0.75 items-center  w-1/2 md:w-full">
-            <Label htmlFor="coverType">Cover Type</Label>
-            <Input id="coverType" type="text" />
-          </div>
-        </div>
 
-        <div className="flex">
-          <div className="py-1.25 mx-3 px-0.75 items-center  w-1/2 md:w-full">
-            <Label htmlFor="price">Price</Label>
-            <Input id="price" type="text" />
+          <div className="flex">
+            <div className="py-1.25 mx-3 px-0.75 items-center  w-1/2 md:w-full">
+              <Label htmlFor="price">Price</Label>
+              <Input
+                id="price"
+                type="text"
+                name="price"
+                onChange={formik.handleChange}
+                value={formik.values.price}
+              />
+              {formik.errors.price && formik.touched.price ? (
+                <Label className="text-red-600">{formik.errors.price}</Label>
+              ) : null}{" "}
+            </div>
+            <div className="py-1.25 mx-3 px-0.75 items-center  w-1/2 md:w-full">
+              <Label htmlFor="quantity">Stock Quantity</Label>
+              <Input
+                id="quantity"
+                type="number"
+                name="quantity"
+                onChange={formik.handleChange}
+                value={formik.values.quantity}
+              />
+              {formik.errors.quantity && formik.touched.quantity ? (
+                <Label className="text-red-600">{formik.errors.quantity}</Label>
+              ) : null}{" "}
+            </div>
           </div>
-          <div className="py-1.25 mx-3 px-0.75 items-center  w-1/2 md:w-full">
-            <Label htmlFor="quantity">Stock Quantity</Label>
-            <Input id="quantity" type="text" />
-          </div>
-        </div>
 
-	<div className="grid gap-2 mx-3">
-          <Label htmlFor="firstName">Upload Image</Label>
-          <Input id="firstName" type="text" />
-        </div>
-      </CardContent>
-      <CardFooter className="flex justify-end my-4">
-        <Button className="mx-3">Add book</Button>
-      </CardFooter>
+          <div className="grid gap-2 my-3 mx-3 w-1/2 ">
+            <Label htmlFor="picture">Upload Image</Label>
+            <Input id="picture" type="file" />
+          </div>
+        </CardContent>
+        <CardFooter className="flex justify-end my-4">
+          <Button className="mx-3" type="submit">
+            Add book
+          </Button>
+        </CardFooter>
+      </form>
     </Card>
   );
 }
